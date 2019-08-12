fb5cea1b1dc7ab368626f9ac0f4704ad7407e7d0
覃英武 <774405003@qq.com>
2019-08-12 13:15:55 +0800

    MVC三层架构

---
+++ src/CarGUIView.java
@@ -0,0 +1,66 @@
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+import java.io.*;
+import java.net.URL;
+import java.awt.event.*;
+import com.sun.java.swing.plaf.windows.*;
+public class CarGUIView extends JFrame implements View{
+   private JEditorPane editorPane;
+   private JScrollPane imagePane;
+   private JScrollPane textPane;
+   private JSplitPane splitPane;
+   private JLabel imgLabel;
+   private CarModel model;
+
+   public CarGUIView(CarModel cmodel){
+      super("Car information- Observer 1");
+	  model = cmodel;
+      buildUpScrollGUI();
+   }
+   private void buildUpScrollGUI(){
+      imgLabel = new JLabel();
+      imgLabel.setBackground(Color.green);
+	  imgLabel.setMinimumSize(new Dimension(250, 400));
+
+      editorPane = new JEditorPane();
+	  editorPane.setEditable(false);
+
+      imagePane = new JScrollPane(imgLabel);
+	  imagePane.getViewport().setBackground(Color.green);
+
+	  textPane = new JScrollPane(editorPane);
+	  textPane.setMinimumSize(new Dimension(250, 200));
+
+	  splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+	  splitPane.setLeftComponent(imagePane);
+	  splitPane.setRightComponent(textPane);
+
+	  Dimension minimumSize = new Dimension(130, 100);
+	  imagePane.setMinimumSize(minimumSize);
+	  textPane.setMinimumSize(new Dimension(100, 100));
+	  splitPane.setDividerLocation(400);
+	  splitPane.setPreferredSize(new Dimension(500, 300));
+
+	  Container contentPane = getContentPane();
+	  contentPane.add(splitPane);
+	  setSize(600, 300);
+	  setVisible(true);
+   }
+
+    public void update(){
+       try{
+	      URL url = model.getCarFileURL();
+          editorPane.setPage(url);
+          System.out.println("We have been called.");
+	   }
+	   catch (IOException e){
+	      e.printStackTrace();
+       }
+       ImageIcon imIcon = model.getImageIcon();
+       Image temp=imIcon.getImage().getScaledInstance(imgLabel.getWidth(), imgLabel.getHeight(), imIcon.getImage().SCALE_DEFAULT);
+       imIcon=new ImageIcon(temp);
+       imgLabel.setIcon(imIcon);
+	   imgLabel.validate();
+    }
+}
---
+++ src/View.java
@@ -0,0 +1,5 @@
+
+public interface View {
+	 public abstract void update();
+
+}
